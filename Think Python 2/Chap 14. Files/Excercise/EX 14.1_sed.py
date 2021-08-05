f6316c89c324a6ef3ccac60344ec5a1ce45a1023
divingbeetle <lsmjeju@gmail.com>
2021-08-06 01:49:15 +0900

    Solved Ex 14

---
+++ Think Python 2/Chap 14. Files/Excercise/EX 14.1_sed.py
@@ -0,0 +1,54 @@
+"""
+
+*** EX 14.1 ***
+    Write a function called sed that takes as arguments
+    a patten string, a replacement string, and two filenames ;
+    it should read the first file and write the contents into the second file
+    If the pattern string appears anywhere in the file,
+    it should be replaced w/ the replacement string.
+
+    If an error occurs while opening, reading, writing or closing files,
+    your program should catch the exception, print an error message, and exit.
+
+
+"""
+
+
+def sed(file_name1, file_name2, pattern_str, replace_str):
+    try:
+        fin1 = open(file_name1)
+    except:
+        print("Error")
+        return
+
+    try:
+        fin2 = open(file_name2, 'w', encoding='UTF-8')
+    except:
+        print("Error")
+        return
+
+    for line in fin1.readlines():
+        try:
+            fin2.write(line.replace(pattern_str, replace_str))
+        except:
+            print("Error")
+            return
+
+    try:
+        fin1.close()
+    except:
+        print("Error")
+        return
+    try:
+        fin2.close()
+    except:
+        print("Error")
+        return
+
+
+def main():
+    sed("99 bottles of beer.txt", "99 bottles of orange juice.txt", "beer", "orange juice")
+
+
+if __name__ == "__main__":
+    main()
